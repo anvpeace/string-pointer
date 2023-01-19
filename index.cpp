c350ea509bedd537781b2fa99c9ac8c7a111d364
anvpeace <cruzpeacem@gmail.com>
2023-01-18 20:10:42 -0500

    pointer through string working

---
+++ index.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+
+    string alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+    const char *p = alphabets.c_str();
+
+
+     cout << p[1] << endl;
+
+
+
+
+    return 0;
+}
+
+// Exercise 1.
+// 
+// Write a program that prints out in the console the whole alphabet "abcdefghijklmnopqrstuvwxyz".
+// All of the characters from the alphabet should be stored in a 27 element array of chars and
+// printing out of each of those elements should be done with the use of the for loop.
